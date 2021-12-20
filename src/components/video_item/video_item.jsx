69adbc0f8966a11c9bce760689968a753ac668e7
DESKTOP-QATI8C5\ppsd_nl_003 <skylar@purpleseed.co.kr>
2021-12-20 10:53:03 +0900

    2nd commit

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import styles from './video_item.module.css'
 
-const VideoItem = ({ video: { snippet } }) => (
+const VideoItem = (
+  { video: { snippet } }, //props에서도 deconstructing이 가능하다. 인자에서 props안에 있는 video를 열어서 받아올 수 있다. 계속 반복되는 video 안에있는 snippet도 deconstructing이 된다.
+) => (
   <li className={styles.container}>
     <div className={styles.video}>
       <img className={styles.thumbnail} src={snippet.thumbnails.medium.url} alt="Video Thumbnail" />
